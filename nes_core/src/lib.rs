91c6dc33916aedd0ba34cb384adac476db96d629
Gabriel Diaz <diazgabe@outlook.com>
2024-05-14 19:44:23 -0400

    chore: provide self argument to instructions and mark them as unimplemented

--- nes_core/src/lib.rs
+++ nes_core/src/lib.rs
@@ -4,172 +4,284 @@ impl Emu {
     // Instructions referenced from https://www.nesdev.org/obelisk-6502-guide/reference.html.
 
     /// ADC - Add with Carry.
-    fn adc() {}
+    fn adc(&mut self) {
+        unimplemented!();
+    }
 
     /// AND - Logical AND.
-    fn and() {}
+    fn and(&mut self) {
+        unimplemented!();
+    }
 
     /// ASL - Arithmetic Shift Left.
-    fn asl() {}
+    fn asl(&mut self) {
+        unimplemented!();
+    }
 
     /// BCC - Branch if Carry Clear.
-    fn bcc() {}
+    fn bcc(&mut self) {
+        unimplemented!();
+    }
 
     /// BCS - Branch if Carry Set.
-    fn bcs() {}
+    fn bcs(&mut self) {
+        unimplemented!();
+    }
 
     /// BEQ - Branch if Equal.
-    fn beq() {}
+    fn beq(&mut self) {
+        unimplemented!();
+    }
 
     /// BIT - Bit Test.
-    fn bit() {}
+    fn bit(&mut self) {
+        unimplemented!();
+    }
 
     /// BMI - Branch if Minus.
-    fn bmi() {}
+    fn bmi(&mut self) {
+        unimplemented!();
+    }
 
     /// BNE - Branch if Not Equal.
-    fn bne() {}
+    fn bne(&mut self) {
+        unimplemented!();
+    }
 
     /// BPL - Branch if Positive.
-    fn bpl() {}
+    fn bpl(&mut self) {
+        unimplemented!();
+    }
 
     /// BRK - Force Interrupt.
-    fn brk() {}
+    fn brk(&mut self) {
+        unimplemented!();
+    }
 
     /// BVC - Branch if Overflow Clear.
-    fn bvc() {}
+    fn bvc(&mut self) {
+        unimplemented!();
+    }
 
     /// BVS - Branch if Overflow Set.
-    fn bvs() {}
+    fn bvs(&mut self) {
+        unimplemented!();
+    }
 
     /// CLC - Clear Carry Flag.
-    fn clc() {}
+    fn clc(&mut self) {
+        unimplemented!();
+    }
 
     /// CLD - Clear Decimal Mode.
-    fn cld() {}
+    fn cld(&mut self) {
+        unimplemented!();
+    }
 
     /// CLI - Clear Interrupt Disable.
-    fn cli() {}
+    fn cli(&mut self) {
+        unimplemented!();
+    }
 
     /// CLV - Clear Overflow Flag.
-    fn clv() {}
+    fn clv(&mut self) {
+        unimplemented!();
+    }
 
     /// CMP - Compare.
-    fn cmp() {}
+    fn cmp(&mut self) {
+        unimplemented!();
+    }
 
     /// CPX - Compare X Register.
-    fn cpx() {}
+    fn cpx(&mut self) {
+        unimplemented!();
+    }
 
     /// CPY - Compare Y Register.
-    fn cpy() {}
+    fn cpy(&mut self) {
+        unimplemented!();
+    }
 
     /// DEC - Decrement Memory.
-    fn dec() {}
+    fn dec(&mut self) {
+        unimplemented!();
+    }
 
     /// DEX - Decrement X Register.
-    fn dex() {}
+    fn dex(&mut self) {
+        unimplemented!();
+    }
 
     /// DEY - Decrement Y Register.
-    fn dey() {}
+    fn dey(&mut self) {
+        unimplemented!();
+    }
 
     /// EOR - Exclusive OR.
-    fn eor() {}
+    fn eor(&mut self) {
+        unimplemented!();
+    }
 
     /// INC - Increment Memory.
-    fn inc() {}
+    fn inc(&mut self) {
+        unimplemented!();
+    }
 
     /// INX - Increment X Register.
-    fn inx() {}
+    fn inx(&mut self) {
+        unimplemented!();
+    }
 
     /// INY - Increment Y Register.
-    fn iny() {}
+    fn iny(&mut self) {
+        unimplemented!();
+    }
 
     /// JMP - Jump.
-    fn jmp() {}
+    fn jmp(&mut self) {
+        unimplemented!();
+    }
 
     /// JSR - Jump to Subroutine.
-    fn jsr() {}
+    fn jsr(&mut self) {
+        unimplemented!();
+    }
 
     /// LDA - Load Accumulator.
-    fn lda() {}
+    fn lda(&mut self) {
+        unimplemented!();
+    }
 
     /// LDX - Load X Register.
-    fn ldx() {}
+    fn ldx(&mut self) {
+        unimplemented!();
+    }
 
     /// LDY - Load Y Register.
-    fn ldy() {}
+    fn ldy(&mut self) {
+        unimplemented!();
+    }
 
     /// LSR - Logical Shift Right.
-    fn lsr() {}
+    fn lsr(&mut self) {
+        unimplemented!();
+    }
 
     /// NOP - No Operation.
-    fn nop() {}
+    fn nop(&mut self) {
+        unimplemented!();
+    }
 
     /// ORA - Logical Inclusive OR.
-    fn ora() {}
+    fn ora(&mut self) {
+        unimplemented!();
+    }
 
     /// PHA - Push Accumulator.
-    fn pha() {}
+    fn pha(&mut self) {
+        unimplemented!();
+    }
 
     /// PHP - Push Processor Status.
-    fn php() {}
+    fn php(&mut self) {
+        unimplemented!();
+    }
 
     /// PLA - Pull Accumulator.
-    fn pla() {}
+    fn pla(&mut self) {
+        unimplemented!();
+    }
 
     /// PLP - Pull Processor Status.
-    fn plp() {}
+    fn plp(&mut self) {
+        unimplemented!();
+    }
 
     /// ROL - Rotate Left.
-    fn rol() {}
+    fn rol(&mut self) {
+        unimplemented!();
+    }
 
     /// ROR - Rotate Right.
-    fn ror() {}
+    fn ror(&mut self) {
+        unimplemented!();
+    }
 
     /// RTI - Return from Interrupt.
-    fn rti() {}
+    fn rti(&mut self) {
+        unimplemented!();
+    }
 
     /// RTS - Return from Subroutine.
-    fn rts() {}
+    fn rts(&mut self) {
+        unimplemented!();
+    }
 
     /// SBC - Subtract with Carry.
-    fn sbc() {}
+    fn sbc(&mut self) {
+        unimplemented!();
+    }
 
     /// SEC - Set Carry Flag.
-    fn sec() {}
+    fn sec(&mut self) {
+        unimplemented!();
+    }
 
     /// SED - Set Decimal Flag.
-    fn sed() {}
+    fn sed(&mut self) {
+        unimplemented!();
+    }
 
     /// SEI - Set Interrupt Disable.
-    fn sei() {}
+    fn sei(&mut self) {
+        unimplemented!();
+    }
 
     /// STA - Store Accumulator.
-    fn sta() {}
+    fn sta(&mut self) {
+        unimplemented!();
+    }
 
     /// STX - Store X Register.
-    fn stx() {}
+    fn stx(&mut self) {
+        unimplemented!();
+    }
 
     /// STY - Store Y Register.
-    fn sty() {}
+    fn sty(&mut self) {
+        unimplemented!();
+    }
 
     /// TAX - Transfer Accumulator to X.
-    fn tax() {}
+    fn tax(&mut self) {
+        unimplemented!();
+    }
 
     /// TAY - Transfer Accumulator to Y.
-    fn tay() {}
+    fn tay(&mut self) {
+        unimplemented!();
+    }
 
     /// TSX - Transfer Stack Pointer to X.
-    fn tsx() {}
+    fn tsx(&mut self) {
+        unimplemented!();
+    }
 
     /// TXA - Transfer X to Accumulator.
-    fn txa() {}
+    fn txa(&mut self) {
+        unimplemented!();
+    }
 
     /// TXS - Transfer X to Stack Pointer.
-    fn txs() {}
+    fn txs(&mut self) {
+        unimplemented!();
+    }
 
     /// TYA - Transfer Y to Accumulator.
-    fn tya() {}
+    fn tya(&mut self) {
+        unimplemented!();
+    }
 }
 
 #[cfg(test)]
